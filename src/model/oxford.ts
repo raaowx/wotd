04baeabda71d59f8d3e96fecd0932ea3ad01230a
raaowx <raaowx@protonmail.com>
2020-07-08 17:10:33 +0200

    [ADD] Main dictionary interface [ADD] Oxford class to be able to search the WOTD in that dictionary [UPDATE] RAE class to fit new interface architecture

---
+++ src/model/oxford.ts
@@ -0,0 +1,36 @@
+import { Dictionary } from "./dictionary";
+import { Parser, Cherioer } from "../utils/cherioer";
+import { WOTD } from "./wotd"; 
+/** Class containing the information about the [Oxford](https://www.oxfordlearnersdictionaries.com) online dictionary */
+export class Oxford implements Dictionary, Parser {
+  readonly url: string = "https://www.oxfordlearnersdictionaries.com"
+  /**
+   * Finds the word of the day in the HTML web page.
+   * @param html String containing the HTML web page.
+   * @returns WOTD object containing the word of the day. Can return `null`.
+   */
+  findWOTD(html: string): WOTD | null {
+    let $ = Cherioer.convert(html);
+    let name = $('.headword div')?.text();
+    let url = $('.headword')?.attr('href');
+        
+    if (name && url) {
+      return new WOTD(name, url);
+    }
+    return null;
+  }
+  /**
+   * Find the meanings of the word of the day in the HTML web page.
+   * @param html String containing the HTML web page.
+   * @returns Array containing the meanings.
+   */
+  findMeanings(html: string): string[] {
+    let $ = Cherioer.convert(html);
+    let meanings: string[] = [];
+    $('li.sense').each((_, e) => {
+      let m = $(e).clone();
+      meanings.push(m.children('.sensetop').children('.def').text());
+    });
+    return meanings;
+  }
+}
--- src/model/rae.ts
+++ src/model/rae.ts
@@ -1,12 +1,45 @@
-/** Class containing the information about the [RAE](https://rae.es) and the online dictionary. */
-export class RAE {
-  static readonly url: string = "https://dle.rae.es";
+import { Dictionary } from "./dictionary";
+import { Parser, Cherioer } from "../utils/cherioer";
+import { WOTD } from "./wotd";
+/** Class containing the information about the [RAE](https://rae.es) online dictionary. */
+export class RAE implements Dictionary, Parser {
+  readonly url: string = "https://dle.rae.es";
   /**
    * Compose an URL based in input.
    * @param path Path component.
    * @returns URL as string.
    */
-  static getUrlFor(path: string): string {
+  private getUrlFor(path: string): string {
     return this.url + path;
+  }
+  /**
+   * Finds the word of the day in the HTML web page.
+   * @param html String containing the HTML web page.
+   * @returns WOTD object containing the word of the day. Can return `null`.
+   */
+  findWOTD(html: string): WOTD | null {
+    let $ = Cherioer.convert(html);
+    let name = $('#wotd a')?.clone().children().remove().end().text();
+    let path = $('#wotd a')?.attr('href')?.split('?')[0];
+    if (name && path) {
+      return new WOTD(name, this.getUrlFor(path));
+    }
+    return null
+  }
+  /**
+   * Finds the meanings of the word of the day in the HTML web page.
+   * @param html String containing the HTML web page.
+   * @returns Array containing the meanings.
+   */
+  findMeanings(html: string): string[] {
+    let $ = Cherioer.convert(html);
+    let meanings: string[] = [];
+    $('.j').each((_, e) => {
+      let m = $(e).clone();
+      m.children().remove('.n_acep');
+      m.children().remove('abbr');
+      meanings.push(m.text());
+    });
+    return meanings;
   }
 }
