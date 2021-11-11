e3bcd4c390fe154b3efc1212cbda2cedb3a0d841
raaowx <raaowx@protonmail.com>
2021-11-11 22:22:56 +0100

    [FIX] Missing extension declaration

--- src/interfaces/fetcher.ts
+++ src/interfaces/fetcher.ts
@@ -1,4 +1,4 @@
-import { WOTD } from "../model/wotd";
+import { WOTD } from "../model/wotd.js";
 /** Interface for force all dictionaries to implement find methods */
 export interface Fetcher {
   fetch(crawler?: number): Promise<WOTD | null>;
